2b7de150ed5103817e35adbfa634027ce31676ce
artyomkorchagin <steamartem@gmail.com>
2023-12-24 20:23:37 +0300

    updated app.py

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import json
 import classes
 
 from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def to_the_server():
+     return {} # render_template('connect.html')
+
+
+if __name__ == '__main__':
+    app.run()
