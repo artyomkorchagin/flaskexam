10b6c106918e70b7fe9d47168e89df103c3ffc0c
artyomkorchagin <steamartem@gmail.com>
2023-12-24 20:50:33 +0300

    added js css html

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def to_the_server():
-     return {} # render_template('connect.html')
+     return render_template('connect.html')
+
+@app.route('/dick_lol')
+def penis():
+    return "penis music"
 
 
 if __name__ == '__main__':
