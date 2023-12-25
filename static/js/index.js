06843222ad14a463ebce8ddac08b11924749d7c6
Ni-Zzok <s.kostin.14@gmail.com>
2023-12-25 15:45:28 +0300

    get govno ajax

--- static/js/index.js
+++ static/js/index.js
@@ -8,11 +8,29 @@ function send_jopa_data()
         connectType: 'application/json',
         data:
         {
-
         },
         success: function (response)
         {
             document.getElementById("jopa_out").value = document.getElementById("jopaID").value
+        }
+    })
+}
+
+function get_govno_data()
+{
+    $.ajax(
+    {
+        type: 'GET',
+        url: '/get_govno',
+        dataType: 'json',
+        connectType: 'application/json',
+        data:
+        {
+        "id1": document.getElementById("id1").value,
+        "id2": document.getElementById("id2").value,
+        },
+        success: function (response)
+        {
         }
     })
 }
